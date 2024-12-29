020b8b6b8230d6e384b713294c4d00c23b361df7
Ivan Antesic <ivan.v.antesic@gmail.com>
2024-12-29 18:05:53 +0100

    raylib-cpp wrapper setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # marshRL
 Prototype traditional roguelike game focused on exploring pathfinding and satisfying hefty combat. 
+
+## Dependencies
+Made with C++. Uses the popular [Raylib](https://github.com/raysan5/raylib) library (version v5.5) and [raylib-cpp wrapper](https://github.com/RobLoach/raylib-cpp) (version v5.5.0).
+Wrapper is already inlcuded in the project, while raylib has to be installed to compile the project - see instructions on the Raylib page.
+
+.vscode files (for windows setup) and Makefile are included for easier setup.
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,35 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
-#include "raylib.h"
+#include "raylib-cpp.hpp"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+int main() {
+    int screenWidth = 800;
+    int screenHeight = 450;
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+    // Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+    raylib::Texture logo("wabbit_alpha.png");
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+    SetTargetFPS(60);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+    while (!window.ShouldClose())
+    {
+        BeginDrawing();
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
+        window.ClearBackground(RAYWHITE);
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+        // Object methods.
+        logo.Draw(
+            screenWidth / 2 - logo.GetWidth() / 2,
+            screenHeight / 2 - logo.GetHeight() / 2);
+
+        EndDrawing();
+    }
+
+    // UnloadTexture() and CloseWindow() are called automatically.
+
+    return 0;
 }
